98506c1cd0a6cf12881be74e87c29d448979154d
kuzmoyev <kuzmovich.goog@gmail.com>
2018-03-25 23:07:44 +0200

    Add tests

---
+++ beautiful_tests/test_beautiful_date.py
@@ -0,0 +1,43 @@
+import unittest
+
+from beautiful_date import *
+from datetime import date, datetime
+
+
+class TestBeautifulDate(unittest.TestCase):
+
+    def test_date_create_by_month_name(self):
+        self.assertEqual(16 / Oct / 1995, date(day=16, month=10, year=1995))
+        self.assertEqual(May / 19 / 2006, date(day=19, month=5, year=2006))
+        self.assertEqual(29 / Feb / 1996, date(day=29, month=2, year=1996))
+
+        self.assertEqual(4 - Jan - 1970, date(day=4, month=1, year=1970))
+        self.assertEqual(Feb - 7 - 1973, date(day=7, month=2, year=1973))
+        self.assertEqual(29 - Feb - 1992, date(day=29, month=2, year=1992))
+
+    def test_date_create_by_prefix_DMY(self):
+        self.assertEqual(D @ 21 / 12 / 1995, date(day=21, month=12, year=1995))
+        self.assertEqual(D @ 21 - 12 - 1994, date(day=21, month=12, year=1994))
+        self.assertEqual(D @ 29 / 2 / 1988, date(day=29, month=2, year=1988))
+
+    def test_date_create_by_prefix_MDY(self):
+        D = MDY()
+        self.assertEqual(D @ 9 - 23 - 1996, date(day=23, month=9, year=1996))
+        self.assertEqual(D @ 10 / 31 / 1995, date(day=31, month=10, year=1995))
+        self.assertEqual(D @ 2 - 29 - 1984, date(day=29, month=2, year=1984))
+
+    def test_datetime_create_by_month_name(self):
+        self.assertEqual((30 / Jan / 1996)[23], datetime(day=30, month=1, year=1996, hour=23, minute=0))
+        self.assertEqual((Feb - 23 - 1975)[1:22], datetime(day=23, month=2, year=1975, hour=1, minute=22))
+        self.assertEqual((29 / Feb / 1992)[23:59:59],
+                         datetime(day=29, month=2, year=1992, hour=23, minute=59, second=59))
+
+    def test_datetime_create_by_prefix(self):
+        self.assertEqual((D @ 8 - 6 - 1947)[5], datetime(day=8, month=6, year=1947, hour=5, minute=0))
+        self.assertEqual((D @ 22 / 7 / 1938)[20:11], datetime(day=22, month=7, year=1938, hour=20, minute=11))
+        self.assertEqual((D @ 29 - 2 - 1988)[10:11:12],
+                         datetime(day=29, month=2, year=1988, hour=10, minute=11, second=12))
+
+
+if __name__ == '__main__':
+    unittest.main()
---
+++ beautiful_tests/test_beautiful_timedelta.py
@@ -0,0 +1,90 @@
+import unittest
+
+from beautiful_date import *
+from datetime import date, datetime
+
+
+class TestBeautifulTimedelta(unittest.TestCase):
+
+    def test_beautiful_delta_dates(self):
+        d = 5 / Oct / 1995
+
+        self.assertEqual(d + 5 * days, date(day=10, month=10, year=1995))
+        self.assertEqual(d - 10 * days, date(day=25, month=9, year=1995))
+        self.assertEqual(d + 27 * days, date(day=1, month=11, year=1995))
+        self.assertEqual(d + 0.5 * days, date(day=5, month=10, year=1995))
+
+        self.assertEqual(d + 3 * weeks, date(day=26, month=10, year=1995))
+        self.assertEqual(d - 2 * weeks, date(day=21, month=9, year=1995))
+        self.assertEqual(d + 7 * weeks, date(day=23, month=11, year=1995))
+        self.assertEqual(d + 3 / 7 * weeks, date(day=8, month=10, year=1995))
+
+        self.assertEqual(d + 2 * months, date(day=5, month=12, year=1995))
+        self.assertEqual(d - 4 * months, date(day=5, month=6, year=1995))
+        self.assertEqual(d + 5 * months, date(day=5, month=3, year=1996))
+
+        self.assertEqual(d + 3 * years, date(day=5, month=10, year=1998))
+        self.assertEqual(d + 10 * years, date(day=5, month=10, year=2005))
+
+        self.assertEqual(d + 2 * days + 10 * months - 3 * years, date(day=7, month=8, year=1993))
+
+    def test_beautiful_delta_dates_inexact(self):
+        last_day_of_month = 31 / Oct / 1995
+        self.assertEqual(last_day_of_month + 1 * months, date(day=30, month=11, year=1995))
+        self.assertEqual(last_day_of_month + 4 * months, date(day=29, month=2, year=1996))
+        self.assertEqual(last_day_of_month + 4 * months + 1 * years, date(day=28, month=2, year=1997))
+
+    def test_beautiful_delta_datetimes(self):
+        dt = (16 / Oct / 1995)[1:2:3]
+
+        self.assertEqual(dt + 25 * microseconds,
+                         datetime(day=16, month=10, year=1995, hour=1, minute=2, second=3, microsecond=25))
+        self.assertEqual(dt - 6 * 10 ** 5 * microseconds,
+                         datetime(day=16, month=10, year=1995, hour=1, minute=2, second=2, microsecond=4 * 10 ** 5))
+        self.assertEqual(dt + 10 ** 6 * microseconds,
+                         datetime(day=16, month=10, year=1995, hour=1, minute=2, second=4))
+
+        self.assertEqual(dt + 25 * seconds, datetime(day=16, month=10, year=1995, hour=1, minute=2, second=28))
+        self.assertEqual(dt + 65 * seconds, datetime(day=16, month=10, year=1995, hour=1, minute=3, second=8))
+        self.assertEqual(dt - 50 * seconds, datetime(day=16, month=10, year=1995, hour=1, minute=1, second=13))
+        self.assertEqual(dt + 0.5 * seconds,
+                         datetime(day=16, month=10, year=1995, hour=1, minute=2, second=3, microsecond=5 * 10 ** 5))
+
+        self.assertEqual(dt + 13 * minutes, datetime(day=16, month=10, year=1995, hour=1, minute=15, second=3))
+        self.assertEqual(dt + 65 * minutes, datetime(day=16, month=10, year=1995, hour=2, minute=7, second=3))
+        self.assertEqual(dt - 20 * minutes, datetime(day=16, month=10, year=1995, hour=0, minute=42, second=3))
+        self.assertEqual(dt + 1 / 6 * minutes, datetime(day=16, month=10, year=1995, hour=1, minute=2, second=13))
+
+        self.assertEqual(dt + 2 * hours, datetime(day=16, month=10, year=1995, hour=3, minute=2, second=3))
+        self.assertEqual(dt + 30 * hours, datetime(day=17, month=10, year=1995, hour=7, minute=2, second=3))
+        self.assertEqual(dt - 13 * hours, datetime(day=15, month=10, year=1995, hour=12, minute=2, second=3))
+        self.assertEqual(dt + 1 / 4 * hours, datetime(day=16, month=10, year=1995, hour=1, minute=17, second=3))
+
+        self.assertEqual(dt + 3 * hours + 5 * minutes - 25 * seconds,
+                         datetime(day=16, month=10, year=1995, hour=4, minute=6, second=38))
+
+        dt += 3 * hours + 5 * minutes - 25 * seconds
+        self.assertEqual(dt, datetime(day=16, month=10, year=1995, hour=4, minute=6, second=38))
+
+    def test_beautiful_setter(self):
+        dt = (16 / Oct / 1995)[1:2:3]
+
+        self.assertEqual(dt << 5 * day, datetime(day=5, month=10, year=1995, hour=1, minute=2, second=3))
+        self.assertEqual(dt << 3 * month, datetime(day=16, month=3, year=1995, hour=1, minute=2, second=3))
+        self.assertEqual(dt << 1998 * year, datetime(day=16, month=10, year=1998, hour=1, minute=2, second=3))
+
+        self.assertEqual(dt << 25 * microsecond,
+                         datetime(day=16, month=10, year=1995, hour=1, minute=2, second=3, microsecond=25))
+        self.assertEqual(dt << 55 * second, datetime(day=16, month=10, year=1995, hour=1, minute=2, second=55))
+        self.assertEqual(dt << 56 * minute, datetime(day=16, month=10, year=1995, hour=1, minute=56, second=3))
+        self.assertEqual(dt << 22 * hour, datetime(day=16, month=10, year=1995, hour=22, minute=2, second=3))
+
+        self.assertEqual(dt << 1919 * year << 5 * month << 4 * day << 23 * hour << 41 * minute << 34 * second,
+                         datetime(day=4, month=5, year=1919, hour=23, minute=41, second=34))
+
+        dt <<= 1919 * year << 5 * month << 4 * day << 23 * hour << 41 * minute << 34 * second
+        self.assertEqual(dt, datetime(day=4, month=5, year=1919, hour=23, minute=41, second=34))
+
+
+if __name__ == '__main__':
+    unittest.main()
